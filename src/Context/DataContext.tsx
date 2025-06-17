f491fd89809c7ce19ea311202a893974a009a380
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-06-17 15:08:19 +0200

    getDate function

--- src/Context/DataContext.tsx
+++ src/Context/DataContext.tsx
@@ -15,6 +15,10 @@ type IDataContext = {
   data: IVenda[] | null;
   loading: boolean;
   error: string | null;
+  inicio: string;
+  setInicio: React.Dispatch<React.SetStateAction<string>>;
+  final: string;
+  setFinal: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const DataContext = React.createContext<IDataContext | null>(null);
@@ -25,12 +29,30 @@ export const useData = () => {
   return context;
 };
 
+function getDate(n: number) {
+  // yyyy-mm-dd
+  const date = new Date();
+  date.setDate(date.getDate() - n);
+  const dd = String(date.getDate()).padStart(2, "0");
+  const mm = String(date.getMonth() + 1).padStart(2, "0");
+  const yyyy = date.getFullYear();
+  // console.log({ dd, mm, yyyy });
+
+  return `${yyyy}-${mm}-${dd}`;
+}
+
 export const DataContextprovider = ({ children }: React.PropsWithChildren) => {
+  const [inicio, setInicio] = React.useState(getDate(30));
+  const [final, setFinal] = React.useState(getDate(0));
+
   const { data, loading, error } = useFetch<IVenda[]>(
-    "https://data.origamid.dev/vendas/"
+    `https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`
   );
+
   return (
-    <DataContext.Provider value={{ data, loading, error }}>
+    <DataContext.Provider
+      value={{ data, loading, error, inicio, setInicio, final, setFinal }}
+    >
       {children}
     </DataContext.Provider>
   );
